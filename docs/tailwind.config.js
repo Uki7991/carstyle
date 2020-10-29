af2d208286e88c0792226626575d557796437f0d
Uki7991 <tilek.kubanov@gmail.com>
2020-10-29 08:50:56 +0600

    docs folder

---
+++ docs/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+        fontFamily: {
+            "carstyle-logo": ['"Secular One"'],
+            "carstyle-text": ['Inter'],
+        },
+        spacing: {
+            '7': '1.75rem',
+            '9': '2.25rem',
+            '21': '5.25rem',
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
